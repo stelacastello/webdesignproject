2b9916542b965e6d3aa517129e4e3687d8b9c61e
BrambleSG <162477694+BrambleSG@users.noreply.github.com>
2024-04-19 21:10:50 +0100

    Update and rename HomepageGalleryscript.js to IndexGalleryscript.js

--- JavaScript/IndexGalleryscript.js
+++ JavaScript/IndexGalleryscript.js
@@ -8,7 +8,7 @@
 			  }
 			}
 		
-          <!-- Bootstrap JS and jQuery -->
+        
     src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"
     src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
