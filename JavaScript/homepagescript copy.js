db92ce270a70c454c8ff63990de37f3a8fcb0bfa
BrambleSG <162477694+BrambleSG@users.noreply.github.com>
2024-04-20 15:46:25 +0100

    Add files via upload

---
+++ JavaScript/homepagescript copy.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var spinText = document.getElementById('spinText');
+    spinText.addEventListener('click', function() {
+        this.classList.toggle('spin-animation');
+    });
+});
+
+
+          <!-- Bootstrap JS and jQuery -->
+    src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+  
