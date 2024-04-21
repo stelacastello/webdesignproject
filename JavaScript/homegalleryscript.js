ed62d374b40181cc834fa5b69a51916261169bea
BrambleSG <162477694+BrambleSG@users.noreply.github.com>
2024-04-21 08:40:50 +0100

    Update homegalleryscript.js

--- JavaScript/homegalleryscript.js
+++ JavaScript/homegalleryscript.js
@@ -1,18 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var spinText = document.getElementById('spinText');
-    spinText.addEventListener('click', function() {
-        this.classList.toggle('spin-animation');
-    });
-});
-
-function toggleText(id) {
-			  var textElement = document.getElementById(id);
-			  if (textElement.style.display === "none") {
-				textElement.style.display = "block";
-			  } else {
-				textElement.style.display = "none";
-			  }
-			}
+function toggleText(boxId) {
+    var boxText = document.getElementById(boxId);
+    if (boxText.style.display === "none" || boxText.style.display === "") {
+        boxText.style.display = "block";
+    } else {
+        boxText.style.display = "none";
+    }
+}
  src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"
     src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
